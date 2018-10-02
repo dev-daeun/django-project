a2391396f5fe32e83621ef998ebdca81d2b9fbef
DaEun <kde6260@gmail.com>
2018-10-02 16:00:53 +0900

    polls index뷰 수정

--- app/polls/views.py
+++ app/polls/views.py
@@ -1,9 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import *
+
 
 def index(request):
-    return HttpResponse('Hello, world. This is poll system index')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 
 
 def detail(request, question_id):
