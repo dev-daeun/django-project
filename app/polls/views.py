31e21c05a4ab7eca5c421657f4c0da7b197e093d
DaEun <kde6260@gmail.com>
2018-10-02 17:05:24 +0900

    디테일 뷰 & 404에러페이지 추가

--- app/polls/views.py
+++ app/polls/views.py
@@ -1,17 +1,38 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
 
 from .models import *
 
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
-    return HttpResponse('This is question : %s' % question_id)
+    try:
+        question = get_object_or_404(Question, id=question_id)
+    except Http404:
+        return render(request, '404.html', {
+            'message': 'Question Does not exists.'
+        })
+    # try:
+    #     detail_question = Question.objects.get(
+    #         id=question_id
+    #     )
+    # except Question.DoesNotExist:
+    #     context = {
+    #         'message': 'Question does not exists.'
+    #     }
+    #     raise render(request, '404.html', context)
+    # else:
+    context = {
+        'question': question,
+    }
+    return render(request, 'polls/detail.html', context)
 
 
 def result(request, question_id):
