88f88752002276419f919ba60c2dc6cb2914a51f
DaEun <kde6260@gmail.com>
2018-10-03 18:31:16 +0900

    polls 디테일 뷰, 템플릿 추가

--- app/polls/views.py
+++ app/polls/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse, Http404
-from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.shortcuts import render, reverse, get_object_or_404
 
 from .models import *
 
@@ -30,4 +30,27 @@ def result(request, question_id):
 
 
 def vote(request, question_id):
-    return HttpResponse('voting on question %s' % question_id)
+    try:
+        question = get_object_or_404(Question, id=question_id)
+    except Http404:
+        return render(request, '404.html', {
+            'message': 'Question Does not exists.'
+        })
+    else:
+        try:
+            # request.POST[value] => html 태그의 id 속성에 들어있는 값을 가져옴.
+            selected_choice = get_object_or_404(Choice, id=request.POST['choice'])
+        except (KeyError, Http404):
+            return render(
+                request=request,
+                template_name='polls/detail.html',
+                context={
+                    'question': question,
+                    'error_message': "You didn't select a choice.",
+                })
+        else:
+            selected_choice.votes += 1
+            selected_choice.save()
+            return HttpResponseRedirect(reverse(
+                'polls:results',
+                args=(question_id,)))
