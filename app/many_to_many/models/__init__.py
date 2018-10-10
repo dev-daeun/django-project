2904e74ad6ed6753d19f70daa85b39f02f38ccc3
DaEun <kde6260@gmail.com>
2018-10-10 11:26:27 +0900

    many_to_many 모델 연습 : 재귀적 다대다 관계

--- app/many_to_many/models/__init__.py
+++ app/many_to_many/models/__init__.py
@@ -1,1 +1,2 @@
 from .basic import Topping, Pizza
+from .recursive import FacebookUser
---
+++ app/many_to_many/models/recursive.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+__all__ = (
+    'FacebookUser',
+)
+
+
+class FacebookUser(models.Model):
+    name = models.CharField(
+        max_length=10,
+    )
+    friends = models.ManyToManyField(
+        'self',
+    )
+
+    def __str__(self):
+        return self.name
